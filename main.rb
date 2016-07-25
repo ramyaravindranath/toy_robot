3107c6c4aacc3b0f479ab1b16a9a91e014425233
ramyaravindranath <ramya.ravindranathp@gmail.com>
2016-07-26 09:54:47 +1000

    "Modifies main.rb and adds .gitignore"

--- main.rb
+++ main.rb
@@ -1,7 +1,10 @@
-class Main
+puts "Do you want to run toy_robot?(Y/N)"
+answer = gets.chomp.upcase
+if answer!="Y"
+  exit
+else
   loop do
-    puts "Do you want to run toy_robot?(Y/N)"
-    answer = gets.chomp.upcase
-    break if answer != "Y"
+    puts "Enter the command"
+    command = gets.chomp.upcase
   end
 end
